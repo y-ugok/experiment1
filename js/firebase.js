41aa518a7f875c9f0fc0c63e81d5d1a05688871e
y-ugok <114295071+y-ugok@users.noreply.github.com>
2025-01-31 08:30:10 +0900

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+const apikey = localStorage.getItem("API_KEY");
+
+const firebaseConfig = {
+  apiKey: apikey,
+  authDomain: "listapp-7e716.firebaseapp.com",
+  projectId: "listapp-7e716",
+  storageBucket: "listapp-7e716.firebasestorage.app",
+  messagingSenderId: "805257599242",
+  appId: "1:805257599242:web:1d85c44918e3040f54c2f3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export class Firebase {
+  async loadDefaultList() {
+    const historySnapshot = await getDocs(collection(db, "history-list"));
+    const historyList = historySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      docID: doc.id,
+    }));
+
+    console.log(historyList);
+    sessionStorage.setItem("history-list", JSON.stringify(historyList));
+
+    const selfSnapshot = await getDocs(collection(db, "self-list"));
+    const selfList = selfSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      docID: doc.id,
+    }));
+
+    console.log(selfList);
+    sessionStorage.setItem("self-list", JSON.stringify(selfList));
+
+    const partnerSnapshot = await getDocs(collection(db, "partner-list"));
+    const partnerList = partnerSnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      docID: doc.id,
+    }));
+
+    console.log(partnerList);
+    sessionStorage.setItem("partner-list", JSON.stringify(partnerList));
+
+    loadList();
+    let savedColor = localStorage.getItem("themeColor");
+    applyThemeColor(savedColor);
+  }
+
+  async addData(collectionName, data) {
+    const docRef = await addDoc(collection(db, collectionName), data);
+    return docRef.id;
+  }
+
+  async deleteData(collectionName, docID) {
+    console.log(collectionName, docID);
+    await deleteDoc(doc(db, collectionName, docID));
+  }
+}
